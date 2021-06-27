0b2acd8a1006b2912141d45ae3a85939c068a651
thomas.cancino <thomas.cancino@mail.udp.cl>
2021-06-27 16:00:06 -0400

    add import boostrap

--- critical-hit-front/src/main.js
+++ critical-hit-front/src/main.js
@@ -5,16 +5,12 @@ import App from './App'
 import router from './router'
 import VueAxios from 'vue-axios'
 import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './main.css'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
